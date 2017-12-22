7b510cf80466efa63609b037242bde6b80a4c18c
devyn cyphers <devcon42@gmail.com>
2017-12-21 19:53:14 -0800

    Scafold and set node version right this time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ToolboxComponent } from './toolbox/toolbox.component';
+import { EditorComponent } from './editor/editor.component';
+import { ItemComponent } from './item/item.component';
+import { BuildComponent } from './build/build.component';
+import { ExportComponent } from './export/export.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolboxComponent,
+    EditorComponent,
+    ItemComponent,
+    BuildComponent,
+    ExportComponent
   ],
   imports: [
     BrowserModule
